0781b716a77beea38d4777f4b67844b9ca339fd9
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-08 18:14:41 +0530

    Time: 50 ms (56.18%) | Memory: 14 MB (77.64%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-    //Memoization
-    int f(int i, int j, string &p, string &s,vector<vector<int>> &dp){
-        if(i < 0 && j < 0)return true;
-        if(i < 0 && j >= 0)return false;
-        if(j < 0 && i >= 0){
-            for(int k = 0; k <= i; k++){
-                if(p[k] != '*')return false;
-
-            }
-            return true;
-        }
-
-        if(dp[i][j] != -1)return dp[i][j];
-        if(p[i] == s[j] || p[i] == '?')return dp[i][j] = f(i-1,j-1,p,s,dp);
-
-        if(p[i] == '*'){
-            return dp[i][j] = f(i-1,j,p,s,dp) || f(i,j-1,p,s,dp);
-        }
-
-        return dp[i][j] = false;
-    }
+    //Tabulation
 
     bool isMatch(string s, string p) {
         int m = s.length();
         int n = p.length();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
 
-        return f(n-1,m-1,p,s,dp);  
+        dp[0][0] = 1;
+        for(int j = 1; j <= m; j++){
+            dp[0][j] = 0;
+        }
+        for(int i = 1; i <= n; i++){
+            int flag = true;
+            for(int k = 1; k <= i; k++){
+                if(p[k-1] != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;    
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(p[i-1] == s[j-1] || p[i-1] == '?') dp[i][j] = dp[i-1][j-1];
+
+                else if(p[i-1] == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+
+                else dp[i][j] = false;
+
+            }
+        }
+
+        return dp[n][m];  
     }
 
 };
