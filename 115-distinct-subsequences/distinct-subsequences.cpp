5ed7d78a035e5df4132dd169c4a9dcee13999da0
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-08 11:10:22 +0530

    Time: 31 ms (52.41%) | Memory: 44.2 MB (40.48%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    //Recursive 
-    int ways(int i, int j, string &s, string &t,vector<vector<int>> &dp){
-        if(j < 0)return 1;
-        if(i < 0)return 0;
-        
-        if(dp[i][j] != -1)return dp[i][j];
-
-        if(s[i] == t[j])return dp[i][j] = ways(i-1,j-1,s,t,dp) + ways(i-1,j,s,t,dp);
-        return dp[i][j] = ways(i-1,j,s,t,dp);
-    }
-
+    //tabulation
     int numDistinct(string s, string t) {
         int m = s.length();
         int n = t.length();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return ways(m-1,n-1,s,t,dp);
+        vector<vector<unsigned long long>> dp(m+1,vector<unsigned long long>(n+1,0));
+
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return (int)dp[m][n];
     }
 };
