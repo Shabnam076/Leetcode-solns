a29547b57a43be6a1954d9930cbdbd24efa2eb9c
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-08 13:58:01 +0530

    Time: 11 ms (28.91%) | Memory: 13.3 MB (41.48%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    //memoisation 
-    int opn(int i, int j, string &word1, string &word2,vector<vector<int>> &dp){
-        if(j == 0){
-            return i;
-        }
-        if(i == 0)return j;
-
-        if(dp[i][j] != -1)return dp[i][j];
-
-        int replace = INT_MAX;
-        int del = INT_MAX;
-        int insert = INT_MAX;
-        
-        if(word1[i-1] == word2[j-1]){
-            return dp[i][j] = opn(i-1, j-1, word1, word2,dp);    
-        }
-        else{
-            replace = 1 + opn(i-1,j-1,word1,word2,dp);
-            del = 1 + opn(i-1,j,word1,word2,dp);
-            insert = 1 + opn(i,j-1,word1,word2,dp);
-        }
-
-        return dp[i][j] = min(del, min(replace, insert));
-    }
+    //Tabulation
     int minDistance(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
 
-        return opn(m,n,word1,word2,dp); 
+        for(int i = 0; i <= n; i++){   //0th row
+            dp[0][i] = i;
+        }
+        for(int i = 0; i <= m; i++){   //0th column
+            dp[i][0] = i;
+        }
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];    
+                }
+                else{
+                    int replace = 1 + dp[i-1][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int insert = 1 + dp[i][j-1];
+                    dp[i][j] = min(replace,min(del,insert));
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
