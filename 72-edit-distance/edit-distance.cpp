7e0badfb43186f169f615769cad452a0dbca6fad
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-08 14:15:43 +0530

    Time: 7 ms (61.10%) | Memory: 10.1 MB (99.71%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    //2d optimised
+    //1d optimised : left ->right
+    //right->left wouldnt work because we require curr[j-1] for calculating curr[j], so processing has to be done from left->right
     int minDistance(string word1, string word2) {
-        int m = word1.length();
-        int n = word2.length();
-        vector<int> prev(n+1,0),curr(n+1,0);
+    int m = word1.size();
+    int n = word2.size();
 
-        for(int i = 0; i <= n; i++){   //0th row
-            prev[i] = i;
-        }
+    vector<int> dp(n + 1);
 
-        for(int i = 1; i <= m; i++){
-            curr[0] = i;              //imp
+    for(int j = 0; j <= n; j++)
+        dp[j] = j;
 
-            for(int j = 1; j <= n; j++){
-                if(word1[i-1] == word2[j-1]){
-                    curr[j] = prev[j-1];    
-                }
-                else{
-                    int replace = 1 + prev[j-1];
-                    int del = 1 + prev[j];
-                    int insert = 1 + curr[j-1];
-                    curr[j] = min(replace,min(del,insert));
-                }
+    for(int i = 1; i <= m; i++) {
+
+        int prevDiag = dp[0]; // dp[i-1][0]
+        dp[0] = i;
+
+        for(int j = 1; j <= n; j++) {
+
+            int temp = dp[j]; // old dp[i-1][j]
+
+            if(word1[i-1] == word2[j-1]) {
+                dp[j] = prevDiag;
             }
-            prev = curr;
-        }
+            else {
+                dp[j] = 1 + min({
+                    prevDiag, // dp[i-1][j-1]
+                    temp,     // dp[i-1][j]
+                    dp[j-1]   // dp[i][j-1]
+                });
+            }
 
-        return prev[n];
+            prevDiag = temp;
+        }
     }
+
+    return dp[n];
+}
 };
