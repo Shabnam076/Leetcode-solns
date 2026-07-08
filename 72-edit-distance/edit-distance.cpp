6caa07a27c1db8804279d6ecd3efbf51555cd3f1
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-08 14:04:38 +0530

    Time: 2 ms (93.76%) | Memory: 10.4 MB (93.77%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    //Tabulation
+    //2d optimised
     int minDistance(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        vector<int> prev(n+1,0),curr(n+1,0);
 
         for(int i = 0; i <= n; i++){   //0th row
-            dp[0][i] = i;
-        }
-        for(int i = 0; i <= m; i++){   //0th column
-            dp[i][0] = i;
+            prev[i] = i;
         }
 
         for(int i = 1; i <= m; i++){
+            curr[0] = i;              //imp
+
             for(int j = 1; j <= n; j++){
                 if(word1[i-1] == word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];    
+                    curr[j] = prev[j-1];    
                 }
                 else{
-                    int replace = 1 + dp[i-1][j-1];
-                    int del = 1 + dp[i-1][j];
-                    int insert = 1 + dp[i][j-1];
-                    dp[i][j] = min(replace,min(del,insert));
+                    int replace = 1 + prev[j-1];
+                    int del = 1 + prev[j];
+                    int insert = 1 + curr[j-1];
+                    curr[j] = min(replace,min(del,insert));
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+
+        return prev[n];
     }
 };
