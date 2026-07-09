78544a735697fc4417b6c4dcad2e71d83c800e73
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-09 13:06:17 +0530

    Time: 4 ms (40.47%) | Memory: 20 MB (65.74%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    //space optimization - 2 arrays
+    //space optimization - 4 variables
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<int> curr(2,0),next(2,0);
-        //base case
-        next[0] = next[1] = 0;
-
+        int nextNotBuy = 0, nextBuy = 0;       //i+1 th row
+        int currNotBuy = 0, currBuy = 0;     //i th row 
+        
         for(int i = n-1; i >= 0; i--){
-            for(int buy = 0; buy <= 1; buy++){
-                int profit = 0;
-                if(buy){
-                    profit = max(-prices[i] + next[0], next[1]);
-                }
+            currBuy = max(-prices[i] + nextNotBuy, nextBuy);   //buy = 1
 
-                else{
-                    profit = max(prices[i] + next[1],next[0]);
-                }
-               curr[buy] = profit;
+            currNotBuy = max(prices[i] + nextBuy,nextNotBuy);   //buy = 0
 
-            }  
-            next = curr; 
+            nextNotBuy = currNotBuy;
+            nextBuy = currBuy;  
         }
 
-        return next[1];
+        return nextBuy;
     }
 };
