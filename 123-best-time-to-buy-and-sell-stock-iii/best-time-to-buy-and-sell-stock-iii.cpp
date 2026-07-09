c18e5dca2b4043051440496d3217ecd1cffbb833
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-09 13:44:31 +0530

    Time: 402 ms (45.31%) | Memory: 235.4 MB (6.05%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,6 +5,10 @@ public:
         int n = prices.size();
         vector<vector<vector<long long>>> dp(n+1,vector<vector<long long>>(2,vector<long long>(3,0)));
 
+        //since dp is initialized with 0, base case is covered already, we just have to make sure that we dont
+        ///overwrite it: so i : 0 to n-1, buy = 0 to 1, cap = 1 to 2.
+        //i == n and cap = 0 is the base case so we will not run the code for these.
+
         for(int i = n-1; i >= 0; i--){
             for(int buy = 0; buy <= 1; buy++){
                 for(int cap = 1; cap <= 2; cap++){
