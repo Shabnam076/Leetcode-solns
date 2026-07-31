b5cb3215934c7ae7d14e42d1af0d550e0d154042
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-07-31 14:06:15 +0530

    Time: 329 ms (70.18%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
-Select c.class from 
+#using distinct
+Select DISTINCT c.class from 
 Courses c
 JOIN (
      Select class, Count(student) as cnt
      from Courses group by class having cnt >= 5
     ) s
-ON c.class = s.class
-group by class;
+ON c.class = s.class;
+
