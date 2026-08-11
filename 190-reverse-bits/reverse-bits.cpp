c57f4a7d90bea823557981e79805c2ce948b3daa
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-11 18:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (13.71%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+
+    //optimal - O(1), O(1)
+    int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            int lsb = n & 1;
+            result = result << 1; //making slot for new lsb
+
+            result = result | lsb; //will append lsb in res
+            n = n >> 1;
+        } 
+
+        return result;
+    }
+
+    /*
     int reverseBits(int n) {
         vector<int> ans(32,0);
         int i = 0;
@@ -16,7 +32,7 @@ public:
             num += (mul* ans[i]);
             mul *= 2;
         }
-        return num;
-        
+        return num; 
     }
+    */
 };
