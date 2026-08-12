514404cb680b667f3d16550556d437bd5eceba97
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-12 11:56:16 +0530

    Time: 627 ms (6.14%) | Memory: 296.4 MB (27.76%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    //Tabulation
+    //Memoization
+    int f(int i, int last, vector<int> &nums,vector<vector<int>> &dp){
+        int n = nums.size();
+        //base case
+        if(i == n) return 0;
+        
+        if(dp[i][last] != -1)return dp[i][last];
+
+        int take = 0;
+        if(last == n || nums[i] > nums[last]) take = 1 + f(i+1, i, nums,dp);
+        int nt = f(i+1, last, nums,dp);
+
+        return dp[i][last] = max(take, nt);
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+2, vector<int>(n+2,0));
-
-        for(int i = n; i >= 1; i--){
-            for(int last = 0; last < i; last++){
-                int take = 0;
-                if(last == 0 || nums[i-1] > nums[last-1]) take = 1 + dp[i+1][i];
-                int nt = dp[i+1][last];
-
-                dp[i][last] = max(take,nt);
-            }
-        }
-        return dp[1][0];
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return f(0, n, nums,dp);
     }
 };
