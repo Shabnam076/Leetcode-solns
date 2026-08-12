e2a539af7afb0a4462e73f385d3f4f9fe994844d
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-12 14:18:18 +0530

    Time: 26 ms (65.61%) | Memory: 27.3 MB (70.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    //memoization - O(n*m), O(n*m) + O(n+m)
-    int f(int i, int j, string &text1, string &text2,vector<vector<int>> &dp){
-        int n = text1.length();
-        int m = text2.length();
-
-        if(i >= n || j >= m)return false;
-
-        if(dp[i][j] != -1)return dp[i][j];
-
-        int same = 0;
-        if(text1[i] == text2[j]){     //both char same
-            same = 1 + f(i+1,j+1,text1,text2,dp);
-        }
-        else{   //both char not same
-            same = max(f(i,j+1,text1,text2,dp), f(i+1,j,text1,text2,dp));
-        }
-
-        return dp[i][j] = same;
-    }
+    //tabulation - O(n*m), O(n*m)
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(0,0,text1,text2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+                int same = 0;
+                if(text1[i] == text2[j]){     //both char same
+                    same = 1 + dp[i+1][j+1];
+                }
+                else{   //both char not same
+                    same = max(dp[i][j+1], dp[i+1][j]);
+                }
+                dp[i][j] = same;
+            }
+        }
+        return dp[0][0];
     }
 };
