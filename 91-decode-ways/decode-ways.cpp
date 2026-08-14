0ddc53c5892af698aa681948ebcf3fdf98734c3f
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-14 18:23:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (26.40%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+//tabulation
 typedef long long int ll;
-
-    ll ways(int i, string &s, vector<int> &dp){
-        int n = s.length();
-        if(i == n) return 1;
-        if(s[i] == '0')return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        ll take1 = ways(i+1,s,dp);  //take 1 digit starting from i 
-
-        ll take2 = 0;
-        ll num = stoi(s.substr(i,2)); //take 2 digit starting from i
-        if(i+1 <= n-1 && (num >= 10 && num <= 26)) take2 = ways(i+2,s,dp);
-
-        return dp[i] = take1 + take2;
-    }
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n,-1);
-        return ways(0,s,dp);  
+        vector<int> dp(n+1,-1);
+        dp[n] = 1;
+
+        for(int i = n-1; i >= 0; i--){
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+
+            ll take1 = dp[i+1];  //take 1 digit starting from i 
+
+            ll take2 = 0;
+            ll num = stoi(s.substr(i,2)); //take 2 digit starting from i
+            if(i+1 <= n-1 && (num >= 10 && num <= 26)) take2 = dp[i+2];
+
+            dp[i] = take1 + take2;
+        }
+
+        return dp[0];  
     }
 };
