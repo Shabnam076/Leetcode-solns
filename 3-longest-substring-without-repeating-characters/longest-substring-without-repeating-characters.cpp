7dfe77257ad7c5bcfef8eee748e28fc6a3e90841
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-27 22:09:29 +0530

    Time: 46 ms (34.65%) | Memory: 19.2 MB (38.80%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
+    //Further optimized only if interviewer ask to 
+    //O(n) time and O(k) space
     int lengthOfLongestSubstring(string s) {
         int n = s.length();
-        unordered_map<int,int> mp;
+        unordered_map<char,int> mp;
+
+        //sliding window
+        int l = 0, r = 0, maxi = 0;
+        while(r < n){
+            if(mp.find(s[r]) != mp.end()){
+                l = max(l, mp[s[r]] + 1);    // why max? - // try on this string = 'abba'
+            }
+            mp[s[r]] = r;
+            maxi = max(maxi, r-l+1);
+            r++;
+        }
+
+        return maxi;
+    }
+
+    /*
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        unordered_map<char,int> mp;
 
         //sliding window
         int l = 0, r = 0, maxi = 0;
@@ -19,5 +40,6 @@ public:
 
         return maxi;
     }
+    */
 
 };
