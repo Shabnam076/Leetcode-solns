07c91a8812a1dec50941973ee8ce7e6d8d8a710e
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-28 00:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.14%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+
+    //Follow up 
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        if(x == 0) return true;
+        
+        int rev = 0;
+        int ori = x;
+
+        while(x){
+            int rem = x % 10;
+
+            if(rev > INT_MAX / 10) return false;
+            if(rev == INT_MAX/10 && rem > INT_MAX % 10)return false;
+
+            rev = rev * 10 + rem;
+            x /= 10;
+        }
+
+        return ori == rev;
+    }
+
+    /*
+    // Time: O(N), Space: O(N)  , N -> no of digits in x.
+    //can call it O(1) as well as at max 10 digits can be in INT_MAX.
     bool isPalindrome(int x) {
         if(x < 0) return false;
         if(x == 0) return true;
@@ -14,4 +39,5 @@ public:
 
         return true;
     }
+    */
 };
