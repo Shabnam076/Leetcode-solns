10a7f35c99703ed30acef4526796be0552f1c440
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-29 12:14:02 +0530

    Time: 29 ms (20.33%) | Memory: 29.3 MB (18.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
+    //Optimzed : Tc : O(N*K), N = total strings , K -> len of each string
+                //Sc : O(N × K)
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<vector<string>> ans;
 
+        unordered_map<string, vector<string>> mp; //0(n*k)
+        for(int i = 0; i < n; i++){
+            string cur = strs[i];
+            int hash[26] = {0};
+            for(char c : cur){     //O(1)
+                hash[c - 'a']++;
+            }
+
+            string key = "";
+            for(int i = 0; i < 26; i++){    //O(1)
+                key += hash[i];
+            }
+
+            mp[key].push_back(cur);
+        }
+
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+
+    /*
+    //Better
     //Tc : O(N × K log K), K -> length of each string, N =  total no of elements in vector
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
@@ -20,6 +50,7 @@ public:
 
         return ans;
     }
+    */
     
     /*
     // Tc : anagram checking : O(n3) + erase opn : O(n2) , overall = O(n3)
