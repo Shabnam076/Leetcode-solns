0fee937657947ff92fc38544628a69907eb87fcb
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-29 14:14:32 +0530

    Time: 32 ms (28.57%) | Memory: 11.3 MB (38.12%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,5 +1,59 @@
 class Solution {
 public:
+    
+    //DP
+    // TC : O(N2) , SC : O(N2)
+    int countSubstrings(string s) {
+        int n = s.length();
+        int cnt = 0;
+
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+
+        for(int l = 1; l <= n; l++){
+            for(int i = 0; i + l - 1 < n; i++){
+                int j = i + l - 1;
+                if(i == j) dp[i][j] = true;
+                else if(i+1 == j){
+                    dp[i][j] = (s[i] == s[j]);
+                }
+                else{
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+                }
+
+                if(dp[i][j] == true) cnt++;
+            }
+        }
+        return cnt;
+    }
+    /*
+
+    // TC: O(N²) — N possible centers × up to O(N) expansion
+    // SC: O(1) — sirf pointers aur counters use ho rahe hain
+    int expand(int i, int j, string &s){
+        int cnt = 0;
+        int n = s.length();
+
+        while(i >= 0 && j < n && s[i] == s[j]){
+            i--; j++; cnt++;
+        }
+
+        return cnt;
+    }
+
+    int countSubstrings(string s) {
+        int n = s.length();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            cnt += expand(i,i,s);
+            if(i + 1 < n) cnt += expand(i,i+1,s);
+        }
+        return cnt;
+    }
+    */
+
+/*
+    //Brute : ~O(n3), SC : O(N)
     bool isPalindrome(string st){
         int l = 0, r = st.length()-1;
 
@@ -8,7 +62,6 @@ public:
             l++; r--;
         }
         return true;
-
     }
 
     int countSubstrings(string s) {
@@ -24,4 +77,5 @@ public:
         }
         return cnt;
     }
+    */
 };
