8bfdd663032b5d35643b7f33a8d228f7812a60f7
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-29 14:15:44 +0530

    Time: 3 ms (85.84%) | Memory: 8.7 MB (59.64%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    
-    //DP
-    // TC : O(N2) , SC : O(N2)
-    int countSubstrings(string s) {
-        int n = s.length();
-        int cnt = 0;
-
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
-
-        for(int l = 1; l <= n; l++){
-            for(int i = 0; i + l - 1 < n; i++){
-                int j = i + l - 1;
-                if(i == j) dp[i][j] = true;
-                else if(i+1 == j){
-                    dp[i][j] = (s[i] == s[j]);
-                }
-                else{
-                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
-                }
-
-                if(dp[i][j] == true) cnt++;
-            }
-        }
-        return cnt;
-    }
-    /*
 
     // TC: O(N²) — N possible centers × up to O(N) expansion
     // SC: O(1) — sirf pointers aur counters use ho rahe hain
@@ -50,7 +24,32 @@ public:
         }
         return cnt;
     }
-    */
+    
+    /*
+    //DP
+    // TC : O(N2) , SC : O(N2)
+    int countSubstrings(string s) {
+        int n = s.length();
+        int cnt = 0;
+
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+
+        for(int l = 1; l <= n; l++){
+            for(int i = 0; i + l - 1 < n; i++){
+                int j = i + l - 1;
+                if(i == j) dp[i][j] = true;
+                else if(i+1 == j){
+                    dp[i][j] = (s[i] == s[j]);
+                }
+                else{
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+                }
+
+                if(dp[i][j] == true) cnt++;
+            }
+        }
+        return cnt;
+    }*/
 
 /*
     //Brute : ~O(n3), SC : O(N)
