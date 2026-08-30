dd1a882e8bff2494fc4e9b1c77d0bf1308238dcc
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-30 18:12:57 +0530

    Time: 822 ms (22.01%) | Memory: 11.6 MB (37.25%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,5 +1,46 @@
 class Solution {
 public:
+    
+    //Brute : O(mn × 4^L) time, O(L) space where L = recursion stack space
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(dfs(i, j, 0, board, word))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(int i, int j, int k, vector<vector<char>> & board, string word){
+        int m = board.size();
+        int n = board[0].size();
+        int l = word.length();
+
+    
+        if(i < 0 || i >= m || j < 0 || j >= n || k >= l) return false;
+
+        if(board[i][j] != word[k]) return false;
+        if(k == l - 1) return true;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        int left = dfs(i,j-1,k+1,board,word);
+        int right = dfs(i,j+1,k+1,board,word);
+        int down = dfs(i+1,j,k+1,board,word);
+        int up = dfs(i-1,j,k+1,board,word);
+
+        board[i][j] = temp;
+
+        return left || right || up || down;
+    }
+
+    /*
+    //Brute : O(mn × 4^L) time, O(mn + L) space
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
@@ -37,4 +78,5 @@ public:
 
         return left || right || up || down;
     }
+    */
 };
