346c224ddeeac674af2f5c7a08a4e62d445fcdf8
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-08-30 18:04:31 +0530

    Time: 862 ms (12.73%) | Memory: 11.8 MB (18.40%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(dfs(i, j, 0, board, word, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(int i, int j, int k, vector<vector<char>> & board, string word,vector<vector<bool>> &visited){
+        int m = board.size();
+        int n = board[0].size();
+        int l = word.length();
+
+    
+        if(i < 0 || i >= m || j < 0 || j >= n || k >= l) return false;
+        if(visited[i][j]) return false;
+
+        if(board[i][j] != word[k]) return false;
+        if(k == l - 1) return true;
+
+        visited[i][j] = 1;
+
+        int left = dfs(i,j-1,k+1,board,word,visited);
+        int right = dfs(i,j+1,k+1,board,word,visited);
+        int down = dfs(i+1,j,k+1,board,word,visited);
+        int up = dfs(i-1,j,k+1,board,word,visited);
+
+        visited[i][j] = 0;
+
+        return left || right || up || down;
+    }
+};
