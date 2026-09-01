b6f8328974db825ce54a58c64ce72dbe3514fef3
Shabnam076 <145551560+Shabnam076@users.noreply.github.com>
2026-09-01 09:16:07 +0530

    Time: 10 ms (11.29%) | Memory: 27.5 MB (33.26%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,25 +11,29 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int> &inorder, int is, int ie, vector<int> &postorder, int ps, int pe, map<int,int> &mp){
-        if(ps > pe || is > ie) return nullptr;
+    TreeNode* build(vector<int>& inorder, int is, int ie, vector<int>& postorder, int ps, int pe, unordered_map<int,int>& mp){
+        if(is > ie || ps > pe) return nullptr;
 
         TreeNode* root = new TreeNode(postorder[pe]);
 
-        int inRoot = mp[postorder[pe]];
-        int numsLeft = inRoot - is;
-        root->left = build(inorder,is,inRoot-1,postorder,ps,ps+numsLeft-1,mp);
-        root->right = build(inorder,inRoot+1,ie,postorder,ps + numsLeft,pe -1 ,mp);
+        int inroot = mp[postorder[pe]];
+        int numsleft = inroot - is;
+
+        root->left = build(inorder, is, inroot-1, postorder, ps, ps + numsleft - 1,mp);
+        root->right = build(inorder, inroot+1, ie, postorder, ps + numsleft, pe-1, mp);
 
         return root;
-
     }
+    
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int, int> mp;
-        for(int i = 0; i < inorder.size(); i++){
+        int n = inorder.size();
+        int m = postorder.size();
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < n; i++){
             mp[inorder[i]] = i;
         }
 
-        return build(inorder,0,inorder.size()-1, postorder,0,postorder.size()-1,mp); 
+        return build(inorder, 0, n-1, postorder, 0, m-1,mp);
     }
 };
